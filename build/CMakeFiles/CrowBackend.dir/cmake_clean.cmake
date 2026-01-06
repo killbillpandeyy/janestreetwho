a82382153497362e74f1d0ffffbaedb2492b78e6
sandeep.bollepalli <sandeep.bollepalli@phonepe.com>
2026-01-07 02:30:47 +0530

    initial commit

---
+++ build/CMakeFiles/CrowBackend.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CrowBackend.dir/main.cpp.o"
+  "CMakeFiles/CrowBackend.dir/main.cpp.o.d"
+  "CrowBackend"
+  "CrowBackend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CrowBackend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CrowBackend.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CrowBackend.
+# This may be replaced when dependencies are built.
